883cc5df31768fd7d62d9e89b1aaf8347ab7951b
Shivamsingh2004 <104717288+Shivamsingh2004@users.noreply.github.com>
2025-04-26 09:00:25 +0000

    data types

---
+++ basic_js/datatypes.js
@@ -0,0 +1,7 @@
+const CardNumber = "2233 4455 6677 8899"
+let UserName = "shivam singh"
+date ="26 march"
+cvv= 891
+console.table([CardNumber,UserName,date,cvv])
+
+// null is  data type define as a object
